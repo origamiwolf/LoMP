6b423e10c54a257238e5a0095d59fc0c96ef3b6f
origamiwolf <origamiwolf@gmail.com>
2026-04-10 19:07:19 +0800

    Updated to 1.12 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         buildConfigField("String", "VERSION_NAME", "\"1.1\"")
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
